d355f7c52126bd68ac640a42b6ed2f10466af30c
Logan Donovan <donovan.236@buckeyemail.osu.edu>
2017-07-19 09:36:17 -0400

    Create pairStar.java

---
+++ CodingBat/Recursion/pairStar.java
@@ -0,0 +1,12 @@
+//Problem: http://codingbat.com/prob/p158175
+
+public String pairStar(String str) {
+  if(str.length() < 2){
+    return str;
+  }
+  if(str.charAt(0) == str.charAt(1)){
+    return str.charAt(0) + "*" + pairStar(str.substring(1));
+  }else{
+    return str.charAt(0) + pairStar(str.substring(1));
+  }
+}
