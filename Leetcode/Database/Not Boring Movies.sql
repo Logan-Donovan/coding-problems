2588ad217aa89a99cbf6206bd68df3f0a3c9462c
Logan Donovan <donovan.236@buckeyemail.osu.edu>
2017-07-28 16:56:16 -0400

    Update Not Boring Movies.sql

--- Leetcode/Database/Not Boring Movies.sql
+++ Leetcode/Database/Not Boring Movies.sql
@@ -1,2 +1,2 @@
-//Problem: https://leetcode.com/problems/not-boring-movies/tabs/description
+--Problem: https://leetcode.com/problems/not-boring-movies/tabs/description
 SELECT * FROM cinema WHERE id%2=1 AND description!='boring' ORDER BY rating DESC;
