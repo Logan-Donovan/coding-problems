108741df05db5edab0ad07b7b7a7b45b4edcc981
Logan Donovan <donovan.236@buckeyemail.osu.edu>
2017-07-28 16:56:05 -0400

    Update Big Countries.sql

--- Leetcode/Database/Big Countries.sql
+++ Leetcode/Database/Big Countries.sql
@@ -1,2 +1,2 @@
-//Problem: https://leetcode.com/problems/big-countries/tabs/description/
+--Problem: https://leetcode.com/problems/big-countries/tabs/description/
 SELECT name,population,area FROM World WHERE population>25000000 OR area>3000000;
