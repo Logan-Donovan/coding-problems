2a0258622db6f6d2e928e30b358e96ed118bc200
Logan Donovan <donovan.236@buckeyemail.osu.edu>
2017-08-01 11:32:48 -0400

    Create Solution.java

---
+++ Leetcode/Algorithms/Single Number/Solution.java
@@ -0,0 +1,11 @@
+//Problem: https://leetcode.com/problems/single-number/discuss/
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++){
+            result ^=nums[i];
+        }
+        return result;
+    }
+}
