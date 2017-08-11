07647045d064162a8131f1b7930b4f760f4f4888
Logan Donovan <donovan.236@buckeyemail.osu.edu>
2017-08-11 08:08:39 -0400

    Update Solution.java

--- Leetcode/Algorithms/Majority Element/Solution.java
+++ Leetcode/Algorithms/Majority Element/Solution.java
@@ -16,3 +16,24 @@ public class Solution {
         return -1;
     }
 }
+
+//alernate, fastest solution:
+
+/*
+public class Solution {
+    public int majorityElement(int[] num) {
+
+        int major=num[0], count = 1;
+        for(int i=1; i<num.length;i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+            
+        }
+        return major;
+    }
+}
+*/
