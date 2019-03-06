2f039a4e9632245195c89c219edf0748dc38b93e
Logan Donovan <logandonovan11@gmail.com>
2019-03-05 22:19:11 -0500

    Create Solution.java

---
+++ Leetcode/Algorithms/Hamming Distance/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int sum = 0;
+        int xor = x^y;
+        while (xor != 0){
+            xor &= (xor -1); //finds and strips the last non-zero bit
+            sum++;
+        }
+        return sum;
+    }
+}
